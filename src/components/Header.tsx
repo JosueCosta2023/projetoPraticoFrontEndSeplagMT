5ac1b8db39bdf520885bb1fb58f1277cc69d7bed
Josue Ocanha Costa <contato_josuecosta@hotmail.com>
2025-04-08 22:09:27 -0400

    feat: link de api em arquivo roteiro

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 const Header = () => {
     return ( 
         <header role="banner" className="text-center w-screen bg-orange-500 p-10">
-          <h1 className="text-4xl font-bold text-black-600">Desaparecidos MT</h1>
+            <h1 className="text-3xl font-bold text-black-600 ">Desaparecidos MT</h1>
           <p className="text-white mt-2 text-xl font-bold">
             Encontre informações sobre pessoas <strong className="bg-slate-800 px-2 rounded-lg">desaparecidas</strong> ou que foram <strong className="bg-blue-800 px-2 rounded-lg">localizadas</strong>. <br />Sua busca pode ajudar uma família.
           </p>
